49a1e3e91762104e0220f9ab63f4c72b04cdc4d2
Nayeem Mustakim <nmustakim98@gmail.com>
2023-12-24 21:36:23 +0600

    appbar updated

--- lib/views/typewise_jobs_screen/typewise_jobs_screen.dart
+++ lib/views/typewise_jobs_screen/typewise_jobs_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:job_estate/utils/size_utils.dart';
 import 'package:job_estate/views/home_screen/widgets/job_card_list.dart';
 import 'package:job_estate/widgets/app_bar/custom_app_bar_job_list.dart';
 
+import '../../constants/image_constant.dart';
 import '../../models/job_model.dart';
+import '../../widgets/app_bar/appbar_leading_image.dart';
+import '../../widgets/app_bar/appbar_subtitle.dart';
+import '../../widgets/app_bar/custom_app_bar.dart';
 
 
 class TypeWiseJobsScreen extends StatelessWidget {
@@ -12,7 +17,18 @@ class TypeWiseJobsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBarForJobList(title: "$title Jobs"),
+        appBar: CustomAppBar(
+            leadingWidth: 50.v,
+            leading: AppbarLeadingImage(
+                imagePath: ImageConstant.imgArrowLeftBlueGray300,
+                margin: EdgeInsets.only(left: 16.h, top: 16.v, bottom: 15.v),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            height: 50.v,
+            title: AppbarSubtitle(
+                text: "$title jobs",
+                margin: EdgeInsets.only(left: 12.h))),
         body: JobCardList(jobsList: []),
      
 
---
+++ lib/widgets/custom_header.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:job_estate/app_export/app_export.dart';
+
+
+class CustomHeader extends StatelessWidget {
+  final String title;
+  const CustomHeader({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 6.0.h),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Text(title,
+            style:theme.textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w500)
+                .copyWith(color: theme.colorScheme.onPrimary.withOpacity(1))),
+
+      ]),
+    );
+  }
+}
